f03ed0c69f48466c99abccfb8d51293bd5d92bd8
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2023-01-03 23:58:39 +0900

    chore : responseMessage 기초 작업 (#9)

--- src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
+++ src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
@@ -1,4 +1,18 @@
 package com.finalteam4.danggeunplanner.common.response;
 
-public class ResponseMessage {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ResponseMessage<T> {
+    private final ResponseEntity<?> responseEntity;
+    private final String msg;
+    private final T data;
+    public ResponseMessage(ResponseEntity<?> responseEntity, String message, T data){
+        this.responseEntity =responseEntity;
+        this.msg = message;
+        this.data = data;
+    }
 }
