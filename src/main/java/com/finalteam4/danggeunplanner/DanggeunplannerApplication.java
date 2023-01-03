1bccb593769fe760232163142695ab4a1f06580e
sungwon <aogn07@naver.com>
2023-01-03 15:06:47 +0900

    chroe: code skeleton
    코드 스켈레톤

---
+++ src/main/java/com/finalteam4/danggeunplanner/DanggeunplannerApplication.java
@@ -0,0 +1,13 @@
+package com.finalteam4.danggeunplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanggeunplannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanggeunplannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/finalteam4/danggeunplanner/DanggeunplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalteam4.danggeunplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanggeunplannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
