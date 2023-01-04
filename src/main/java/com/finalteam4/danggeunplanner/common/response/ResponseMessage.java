254d862b905e53e1082556bee56150ecd265f061
JaeSuk <110963294+suhjaesuk@users.noreply.github.com>
2023-01-04 14:25:41 +0900

    ✨ Feat : 리스폰스 메세지 수정  (#11)
    * chore : responseMessage 기초 작업
    
    * fix : ResponseMessage 수정
    
    * fix : ResponseMessage 수정

--- src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
+++ src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
@@ -2,17 +2,14 @@ package com.finalteam4.danggeunplanner.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import org.springframework.http.ResponseEntity;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseMessage<T> {
-    private final ResponseEntity<?> responseEntity;
-    private final String msg;
+    private final String message;
     private final T data;
-    public ResponseMessage(ResponseEntity<?> responseEntity, String message, T data){
-        this.responseEntity =responseEntity;
-        this.msg = message;
+    public ResponseMessage(String message, T data){
+        this.message = message;
         this.data = data;
     }
 }
