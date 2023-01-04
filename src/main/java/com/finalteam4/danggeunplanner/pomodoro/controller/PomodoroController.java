a63a1474d1d6dc6e180012da4ef1c1b45724bbb4
suhjaesuk <suhjaesuk@gmail.com>
2023-01-04 21:21:38 +0900

    feat : 뽀모도로 완료 구현

--- src/main/java/com/finalteam4/danggeunplanner/pomodoro/controller/PomodoroController.java
+++ src/main/java/com/finalteam4/danggeunplanner/pomodoro/controller/PomodoroController.java
@@ -1,7 +1,12 @@
 package com.finalteam4.danggeunplanner.pomodoro.controller;
 
+import com.finalteam4.danggeunplanner.common.response.ResponseMessage;
 import com.finalteam4.danggeunplanner.pomodoro.service.PomodoroService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/pomodoro")
 public class PomodoroController {
     private final PomodoroService pomodoroService;
+    @PostMapping("/{id}")
+    ResponseEntity<?> finish(@PathVariable Long id){
+        pomodoroService.finish(id);
+        return new ResponseEntity<>(new ResponseMessage<>("뽀모도로 완료",null), HttpStatus.CREATED);
+    }
+
+
 }
--- src/main/java/com/finalteam4/danggeunplanner/pomodoro/service/PomodoroService.java
+++ src/main/java/com/finalteam4/danggeunplanner/pomodoro/service/PomodoroService.java
@@ -1,11 +1,33 @@
 package com.finalteam4.danggeunplanner.pomodoro.service;
 
+import com.finalteam4.danggeunplanner.common.exception.DanggeunPlannerException;
+import com.finalteam4.danggeunplanner.member.entity.Member;
+import com.finalteam4.danggeunplanner.member.repository.MemberRepository;
+import com.finalteam4.danggeunplanner.pomodoro.entity.Pomodoro;
+import com.finalteam4.danggeunplanner.pomodoro.repository.PomodoroRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.NOT_FOUND_MEMBER;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class PomodoroService {
+    private final PomodoroRepository pomodoroRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public void finish(Long id){
+        Member member = validatedMemberById(id);
+        Pomodoro pomodoro = new Pomodoro(member);
+        pomodoroRepository.save(pomodoro);
+    }
+
+    private Member validatedMemberById(Long id){
+        return memberRepository.findById(id).orElseThrow(
+                ()-> new DanggeunPlannerException(NOT_FOUND_MEMBER));
+    }
+
 }
