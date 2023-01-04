5723eeda20d8a1a2a16808587c997bae41df6f6e
suhjaesuk <suhjaesuk@gmail.com>
2023-01-04 11:39:40 +0900

    fix : ResponseMessage 수정

--- src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
+++ src/main/java/com/finalteam4/danggeunplanner/common/response/ResponseMessage.java
@@ -2,16 +2,13 @@ package com.finalteam4.danggeunplanner.common.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
-import org.springframework.http.ResponseEntity;
 
 @Getter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseMessage<T> {
-    private final ResponseEntity<?> responseEntity;
     private final String msg;
     private final T data;
-    public ResponseMessage(ResponseEntity<?> responseEntity, String message, T data){
-        this.responseEntity =responseEntity;
+    public ResponseMessage(String message, T data){
         this.msg = message;
         this.data = data;
     }
