8fae7d7facfa2eed350d94110928ccc2a045fd48
JeongO <110980231+JeongO41@users.noreply.github.com>
2023-01-04 21:13:04 +0900

    ✨ Feat : 단순 회원가입 기능 구현 Feature/signup(#3) (#15)
    * feat : 회원가입 기능 단순 구현 (#3)   
    
    security 없이 단순 회원가입만 가능하도록 구현
    
    * 단순 회원가입 기능 구현
    
    * fix : signUp 로직 패키지 변경(auth -> member), 정적팩토리메서드 삭제
    
    * fix : Dto네임 수정
    
    dto 네임 수정 및  매개 변수 네임 수정

--- src/main/java/com/finalteam4/danggeunplanner/member/service/MemberService.java
+++ src/main/java/com/finalteam4/danggeunplanner/member/service/MemberService.java
@@ -1,13 +1,32 @@
 package com.finalteam4.danggeunplanner.member.service;
 
+import com.finalteam4.danggeunplanner.common.exception.DanggeunPlannerException;
+import com.finalteam4.danggeunplanner.member.dto.request.MemberSignUpRequest;
+import com.finalteam4.danggeunplanner.member.entity.Member;
 import com.finalteam4.danggeunplanner.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.DUPLICATED_EMAIL;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+
+    @Transactional
+    public void signUp(MemberSignUpRequest request) {
+        String email = request.getEmail();
+        String password = request.getPassword();
+
+        memberRepository.findByEmail(email)
+                .ifPresent( m -> {
+                    throw new DanggeunPlannerException(DUPLICATED_EMAIL);
+                });
+
+        Member member = new Member(email, password);
+        memberRepository.save(member);
+    }
 }
