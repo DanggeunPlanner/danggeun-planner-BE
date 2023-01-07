14d9422f6a7e699fc7b24e26b1f0caa0a102e411
JeongO <110980231+JeongO41@users.noreply.github.com>
2023-01-07 19:03:45 +0900

    ✨ Feat : 마이페이지 조회 기능 구현 (#41)
    * feat : 마이페이지 조회 기능 구현
    
    * fix : 코드 수정
    
    마이페이지 조회 기능 - 메서드명, 변수명 수정, 오타수정(email)
    
    * fix : import 오류 수정

--- src/main/java/com/finalteam4/danggeunplanner/timer/repository/TimerRepository.java
+++ src/main/java/com/finalteam4/danggeunplanner/timer/repository/TimerRepository.java
@@ -1,9 +1,11 @@
 package com.finalteam4.danggeunplanner.timer.repository;
 
+import com.finalteam4.danggeunplanner.member.entity.Member;
 import com.finalteam4.danggeunplanner.timer.entity.Timer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TimerRepository extends JpaRepository<Timer, Long> {
     List<Timer> findAllByDateAndId(String date, Long id);
+    List<Timer> findAllByMember(Member member);
 }
