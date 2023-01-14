1f879188086a0d6c057a528efba0383b4006c02b
JeongO <110980231+JeongO41@users.noreply.github.com>
2023-01-15 00:01:37 +0900

    refactor : 전체적인 코드 리팩토링 (#65)
    - Member 도메인 쪽 코드 리팩토링
    - 현재 에러 해결중인 refreshToken 관련 로직은 수정하지 않은 상태임

--- src/main/java/com/finalteam4/danggeunplanner/member/dto/request/MemberInfoRequest.java
+++
@@ -1,7 +0,0 @@
-package com.finalteam4.danggeunplanner.member.dto.request;
-
-import lombok.Getter;
-
-@Getter
-public class MemberInfoRequest {
-}
--- src/main/java/com/finalteam4/danggeunplanner/member/service/MemberValidator.java
+++ src/main/java/com/finalteam4/danggeunplanner/member/service/MemberValidator.java
@@ -2,20 +2,42 @@ package com.finalteam4.danggeunplanner.member.service;
 
 import com.finalteam4.danggeunplanner.common.exception.DanggeunPlannerException;
 import com.finalteam4.danggeunplanner.member.entity.Member;
+import com.finalteam4.danggeunplanner.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.DUPLICATED_EMAIL;
+import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.DUPLICATED_NICKNAME;
 import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.NOT_VALID_ACCESS;
+import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.NOT_VALID_PASSWORD;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberValidator {
-
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
     public void validateAccess(Member member , Member other){
         if(!member.getId().equals(other.getId())){
             throw new DanggeunPlannerException(NOT_VALID_ACCESS);
+        }
+    }
+    public void validateEmail(String email){
+        memberRepository.findByEmail(email)
+                .ifPresent( m -> {
+                    throw new DanggeunPlannerException(DUPLICATED_EMAIL);
+        });
+    }
+    public void validateMatchPassword(String storedPassword, String password){
+        if(!passwordEncoder.matches(storedPassword, password)){
+            throw new DanggeunPlannerException(NOT_VALID_PASSWORD);
+        }
+    }
+    public void validateUsername(String username){
+        if(memberRepository.existsByUsername(username)){
+            throw new DanggeunPlannerException(DUPLICATED_NICKNAME);
         }
     }
 }
