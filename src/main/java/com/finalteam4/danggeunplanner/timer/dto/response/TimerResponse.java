b6c82b546fc3028c28886267a4d42794390f1e3c
suhjaesuk <suhjaesuk@gmail.com>
2023-01-17 15:24:14 +0900

    feat : Timer api 변경에의한 수정

--- src/main/java/com/finalteam4/danggeunplanner/timer/dto/response/TimerResponse.java
+++ src/main/java/com/finalteam4/danggeunplanner/timer/dto/response/TimerResponse.java
@@ -1,19 +1,22 @@
 package com.finalteam4.danggeunplanner.timer.dto.response;
 
+import com.finalteam4.danggeunplanner.TimeConverter;
 import com.finalteam4.danggeunplanner.timer.entity.Timer;
 import lombok.Getter;
 
 @Getter
 public class TimerResponse {
     private final Long timerId;
-    private final String date;
+    private final String content;
     private final String startTime;
     private final String endTime;
+    private final Integer count;
 
     public TimerResponse(Timer timer){
         this.timerId = timer.getId();
-        this.date = timer.getDate();
-        this.startTime = timer.getStartTime();
-        this.endTime = timer.getEndTime();
+        this.content = timer.getContent();
+        this.startTime = TimeConverter.convertToHourMinute(timer.getStartTime());
+        this.endTime = TimeConverter.convertToHourMinute(timer.getEndTime());
+        this.count = timer.getCount();
     }
 }
--- src/main/java/com/finalteam4/danggeunplanner/timer/service/TimerService.java
+++ src/main/java/com/finalteam4/danggeunplanner/timer/service/TimerService.java
@@ -9,14 +9,17 @@ import com.finalteam4.danggeunplanner.member.entity.Member;
 import com.finalteam4.danggeunplanner.member.service.MemberValidator;
 import com.finalteam4.danggeunplanner.planner.entity.Planner;
 import com.finalteam4.danggeunplanner.planner.repository.PlannerRepository;
+import com.finalteam4.danggeunplanner.timer.dto.request.TimerFinishRequest;
+import com.finalteam4.danggeunplanner.timer.dto.request.TimerStartRequest;
+import com.finalteam4.danggeunplanner.timer.dto.request.TimerUpdateRequest;
 import com.finalteam4.danggeunplanner.timer.dto.response.TimerResponse;
+import com.finalteam4.danggeunplanner.timer.dto.response.TimerStartResponse;
 import com.finalteam4.danggeunplanner.timer.entity.Timer;
 import com.finalteam4.danggeunplanner.timer.repository.TimerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static com.finalteam4.danggeunplanner.common.exception.ErrorCode.NOT_FOUND_CALENDAR;
@@ -30,76 +33,80 @@ public class TimerService {
     private final TimerRepository timerRepository;
     private final PlannerRepository plannerRepository;
     private final CalendarRepository calendarRepository;
-    private final TimerValidator timerValidator;
     private final MemberValidator memberValidator;
 
     @Transactional
-    public TimerResponse start(Member member){
-        deleteRunningTimer(member);
-
-        Timer timer = new Timer(member);
+    public TimerStartResponse start(Member member, TimerStartRequest request) {
+        Timer timer = request.toTimer(member);
         timerRepository.save(timer);
-        return new TimerResponse(timer);
+        return new TimerStartResponse(timer);
     }
 
+
     @Transactional
-    public TimerResponse finish(Member member,Long timerId){
+    public TimerResponse finish(Member member, Long timerId, TimerFinishRequest request) {
         Timer timer = timerRepository.findById(timerId).orElseThrow(
-                ()->new DanggeunPlannerException(NOT_FOUND_TIMER)
+                () -> new DanggeunPlannerException(NOT_FOUND_TIMER)
         );
 
-        memberValidator.validateAccess(member,timer.getMember());
-        timerValidator.validateFinish(timer);
-        timerValidator.validateActiveTimer(timer);
+        memberValidator.validateAccess(member, timer.getMember());
 
-        timer.finish(timer);
+        timer.finish(request.getEndTime(), request.getCount());
         deleteInactiveTimer(member);
 
-        Planner planner = plannerRepository.findByMemberAndDate(member,TimeConverter.getCurrentTimeToYearMonthDay()).orElseThrow(
+        createPlanner(member, timer);
+        createCalendar(member, timer);
+
+        Planner planner = plannerRepository.findByMemberAndDate(member, TimeConverter.convertToPlannerDateForm(timer.getEndTime())).orElseThrow(
                 () -> new DanggeunPlannerException(NOT_FOUND_PLANNER)
         );
-        Calendar calendar = calendarRepository.findByMemberAndDate(member,TimeConverter.getCurrentTimeToYearMonth()).orElseThrow(
+        Calendar calendar = calendarRepository.findByMemberAndDate(member, TimeConverter.convertToCalendarDateForm(timer.getEndTime())).orElseThrow(
                 () -> new DanggeunPlannerException(NOT_FOUND_CALENDAR)
         );
 
-        createPlanner(member);
         timer.confirmPlanner(planner);
-        planner.addCarrot();
-
-        createCalendar(member);
         planner.confirmCalendar(calendar);
+
+        planner.addCarrot();
         calendar.addCarrot();
-        
+
+        return new TimerResponse(timer);
+    }
+    @Transactional
+    public TimerResponse update(Member member, Long timerId, TimerUpdateRequest request) {
+        Timer timer = timerRepository.findById(timerId).orElseThrow(
+                () -> new DanggeunPlannerException(NOT_FOUND_TIMER)
+        );
+        memberValidator.validateAccess(member, timer.getMember());
+
+        timer.update(request.getContent());
         return new TimerResponse(timer);
     }
 
-    private void deleteRunningTimer(Member member){
-        List<Timer> timers = timerRepository.findAllByMember(member);
-        for(Timer timer : timers){
-            if(LocalDateTime.now().isBefore(timer.getCreatedAt().plusMinutes(24))){
-                timerRepository.delete(timer);
-            }
-        }
-    }
 
-    private void createPlanner(Member member){
-        if(!plannerRepository.existsByMemberAndDate(member,TimeConverter.getCurrentTimeToYearMonthDay())) {
-            Planner planner = new Planner(member,TimeConverter.getCurrentTimeToYearMonthDay());
+    private void createPlanner(Member member, Timer timer) {
+        String date = TimeConverter.convertToPlannerDateForm(timer.getEndTime());
+
+        if (!plannerRepository.existsByMemberAndDate(member, date)) {
+            Planner planner = new Planner(member, date);
             plannerRepository.save(planner);
         }
     }
 
-    private void createCalendar(Member member){
-        if(!calendarRepository.existsByMemberAndDate(member,TimeConverter.getCurrentTimeToYearMonth())) {
-            Calendar calendar = new Calendar(member);
+    private void createCalendar(Member member, Timer timer) {
+        String date = TimeConverter.convertToCalendarDateForm(timer.getEndTime());
+
+        if (!calendarRepository.existsByMemberAndDate(member, date)) {
+            Calendar calendar = new Calendar(member, date);
             calendarRepository.save(calendar);
         }
     }
 
-    private void deleteInactiveTimer(Member member){
+    private void deleteInactiveTimer(Member member) {
         List<Timer> timers = timerRepository.findAllByMember(member);
-        for(Timer timer : timers){
-            if(!timer.getIsFinish()){
+
+        for (Timer timer : timers) {
+            if (!timer.getIsFinish()) {
                 timerRepository.delete(timer);
             }
         }
